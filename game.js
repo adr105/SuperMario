6b8f75c231dc78ee8b501b43658c210310fd2ba0
adr105 <adriac01@ucm.es>
2019-04-12 19:03:53 +0200

    Reset escenario

--- game.js
+++ game.js
@@ -6,35 +6,6 @@ window.addEventListener("load",function() {
 	        .setup({ width: 320, height: 480})
 	        .controls().touch();
 
-	Q.Sprite.extend("Player",{
-		init: function(x, y, p){
-			this._super(p, {
-				sheet: "mario_small",
-				sprite: "mario_small",
-				speed: 300,
-				jumpSpeed: -450,
-				x: x,
-				y: y,
-				die: false
-
-			});
-			this.p.points = this.p.standingPoints;
-		    this.add('2d, platformerControls');
-
-		},
-		
-		step: function(dt) {
-			
-			if(this.p.y > 500) {
-		       this.stage.unfollow(); 
-		    }
-
-
-		  }	  
-
-	});
-	
-
 
 	Q.loadTMX("level.tmx, mario_small.json, mario_small.png", function(){
 		Q.stageScene("level1");
@@ -43,10 +14,9 @@ window.addEventListener("load",function() {
 
 	Q.scene("level1", function(stage) {
 	  Q.stageTMX("level.tmx", stage);
-	  Mario = stage.insert(new Q.Player(150, 380));
-	  stage.add("viewport").follow(Mario);
-	  stage.viewport.offsetX = -Q.width*30/100;
-  	  stage.viewport.offsetY = Q.height*30/100;
+	  stage.add("viewport");
+	  stage.centerOn(150, 380);
+
 	 });
 
 	
