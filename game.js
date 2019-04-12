69929e91c6649f4c51efdbcad1ae28cf47a652c1
adr105 <adriac01@ucm.es>
2019-04-12 19:28:45 +0200

    Mario on scene

--- game.js
+++ game.js
@@ -6,16 +6,38 @@ window.addEventListener("load",function() {
 	        .setup({ width: 320, height: 480})
 	        .controls().touch();
 
+	/* Mario */
+	Q.Sprite.extend("Player",{
+		init: function(p){
+			this._super(p, {
+				sheet: "mario_small",
+				sprite: "mario_small",
+			    x: 150,
+			    y: 380,
+			});
+	    this.add('2d, platformerControls');
+
+		},
+
+
+	});
+
+
 
 	Q.loadTMX("level.tmx, mario_small.json, mario_small.png", function(){
-		Q.stageScene("level1");
 		Q.compileSheets("mario_small.png","mario_small.json");
+		Q.sheet("mario_small","mario_small.png", { tilew: 32, tileh: 32 });
+		Q.stageScene("level1");
+
 	});
 
 	Q.scene("level1", function(stage) {
 	  Q.stageTMX("level.tmx", stage);
-	  stage.add("viewport");
-	  stage.centerOn(150, 380);
+	  Mario = stage.insert(new Q.Player());
+	  stage.add("viewport").follow(Mario);
+	  stage.viewport.offsetX = -Q.width*30/100;
+  	  stage.viewport.offsetY = Q.height*33/100;
+	  //stage.centerOn(150, 380);
 
 	 });
 
